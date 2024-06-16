18acd01e3c13a4470e295f0bc74c38831b2d48a5
Admin <maayush235@gmail.com>
2024-06-16 14:10:15 +0530

    Radhey Radhey

---
+++ lib/Model/Video_Lists.dart
@@ -0,0 +1,14 @@
+final List<Map<String, String>> videos = [
+  {
+    'title': 'Big Buck Bunny',
+    'url': 'https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4'
+  },
+  {
+    'title': 'Elephant Dream',
+    'url': 'https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4'
+  },
+  {
+    'title': 'Sintel',
+    'url': 'https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/Sintel.mp4'
+  },
+];
--- lib/Views/video_select_screen.dart
+++ lib/Views/video_select_screen.dart
@@ -1,23 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:mx_player_clone/Views/play_Screen.dart';
 
-class VideoSelectionScreen extends StatelessWidget {
-  final List<Map<String, String>> videos = [
-    {
-      'title': 'Big Buck Bunny',
-      'url': 'https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4'
-    },
-    {
-      'title': 'Elephant Dream',
-      'url': 'https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4'
-    },
-    {
-      'title': 'Sintel',
-      'url': 'https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/Sintel.mp4'
-    },
-  ];
+import '../Model/Video_Lists.dart';
 
-  VideoSelectionScreen({Key? key}) : super(key: key);
+class VideoSelectionScreen extends StatelessWidget {
+  const VideoSelectionScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
